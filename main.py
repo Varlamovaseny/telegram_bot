b2fb4c7dc8c26cb1cb7a15f014945d72bfbe8ce6
Varlamova Seny <61526004+Varlamovaseny@users.noreply.github.com>
2023-04-25 22:54:03 +1000

    добавление кнопочек

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 reply_keyboard = [['boy', 'girl']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 user_id = [""]
+data_buttons = ['boy', 'girl', '14-18', '18-25', '25-35', '35+', '+', '-']
+gender = []
+age = []
+
 
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
@@ -43,25 +47,75 @@ async def start(update, context):
     )
 
 
+async def boy(update, context):
+    await update.message.reply_html(rf"Хорошо! Какой возраст тебя интересует?")
+
+
 async def profile(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
+    t = '\n'
+    if update.message.text == data_buttons[0]:
+        reply_keyboard[0] = ['14-18', '18-25', '25-35', '35+']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(rf"Хорошо! Теперь укажи возраст, который тебя интересует.", reply_markup=markup)
+        gender.append(update.message.text)
+        return 0
+
+    elif update.message.text == data_buttons[1]:
+        reply_keyboard[0] = ['14-18', '18-25', '25-35', '35+']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(rf"Хорошо! Теперь укажи возраст, который тебя интересует.", reply_markup=markup)
+        gender.append(update.message.text)
+        return 0
+
+    elif update.message.text == data_buttons[2]:
+        reply_keyboard[0] = ['+', '-']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            rf"Хорошо! Теперь давай начнем искать тебе собеседника :3{t}Нажми - чтобы начать! ", reply_markup=markup)
+        age.append(update.message.text)
+        return 0
+
+    elif update.message.text == data_buttons[3]:
+        reply_keyboard[0] = ['+', '-']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            rf"Хорошо! Теперь давай начнем искать тебе собеседника :3{t}Нажми - чтобы начать! ", reply_markup=markup)
+        age.append(update.message.text)
+        return 0
+
+    elif update.message.text == data_buttons[4]:
+        reply_keyboard[0] = ['+', '-']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            rf"Хорошо! Теперь давай начнем искать тебе собеседника :3{t}Нажми - чтобы начать! ", reply_markup=markup)
+        age.append(update.message.text)
+        return 0
+
+    elif update.message.text == data_buttons[5]:
+        reply_keyboard[0] = ['+', '-']
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            rf"Хорошо! Теперь давай начнем искать тебе собеседника :3{t}Нажми - чтобы начать! ", reply_markup=markup)
+        age.append(update.message.text)
+        return 0
+
     data = update.message.text.split("\n")
-    data = (user_id[0], data[0], data[1], data[2], "\n".join(data[3:]))
+    data = (user_id[0], data[0], data[1], int(data[2]), "\n".join(data[3:]))
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text(
         rf"Готово! Теперь ты можешь начать общение! Выбери, кто тебя интересует больше?", reply_markup=markup
     )
+
     cur.execute("INSERT INTO profiles VALUES(?, ?, ?, ?, ?);", data)
     conn.commit()
+
 
 def main():
     # Создаём объект Application.
     # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("boy", boy))
     # Создаём обработчик сообщений типа filters.TEXT
     # из описанной выше асинхронной функции echo()
     # После регистрации обработчика в приложении
@@ -78,3 +132,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+cur.execute("SELECT * FROM profiles ;")
+three_results = cur.fetchall()
+print(three_results)
